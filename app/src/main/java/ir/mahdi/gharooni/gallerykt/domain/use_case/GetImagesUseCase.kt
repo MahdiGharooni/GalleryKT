eea2aa7c98e3ffb3329dcf3f8d6a10dfc9ae452f
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-11-11 09:37:00 +0330

    imp usecase

---
+++ app/src/main/java/ir/mahdi/gharooni/gallerykt/domain/use_case/GetImagesUseCase.kt
@@ -0,0 +1,19 @@
+package ir.mahdi.gharooni.gallerykt.domain.use_case
+
+import ir.mahdi.gharooni.gallerykt.domain.model.Image
+import ir.mahdi.gharooni.gallerykt.domain.repository.GalleryRepository
+import ir.mahdi.gharooni.gallerykt.utils.Response
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetImagesUseCase @Inject constructor(
+    private val repository: GalleryRepository
+) {
+    suspend operator fun invoke() : Flow<Response<List<Image>>> {
+        return repository.getImages()
+    }
+    //todo: test both
+    suspend fun getImages() : Flow<Response<List<Image>>> {
+        return repository.getImages()
+    }
+}
