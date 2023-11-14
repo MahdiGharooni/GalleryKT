96e56670fc7b90c1d210e4a31cc0171b7f916c90
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-11-14 16:07:44 +0330

    imp viewModel

---
+++ app/src/main/java/ir/mahdi/gharooni/gallerykt/presentation/view_model/GetImagesViewModel.kt
@@ -0,0 +1,56 @@
+package ir.mahdi.gharooni.gallerykt.presentation.view_model
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ir.mahdi.gharooni.gallerykt.domain.use_case.GetImagesUseCase
+import ir.mahdi.gharooni.gallerykt.presentation.state.GetImagesState
+import ir.mahdi.gharooni.gallerykt.utils.Response
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class GetImagesViewModel @Inject constructor(
+    private val useCase: GetImagesUseCase
+) : ViewModel() {
+
+    init {
+        getImages()
+    }
+
+
+    private val _state = mutableStateOf(GetImagesState())
+    val state: State<GetImagesState> = _state
+
+
+    private fun getImages() {
+        viewModelScope.launch {
+            useCase().collect {
+                when (it) {
+                    is Response.Loading<*> -> {
+                        _state.value = GetImagesState(
+                            isLoading = true,
+                        )
+                    }
+
+                    is Response.Error<*> -> {
+                        _state.value = GetImagesState(
+                            isLoading = false,
+                            message = it.message ?: "something is wrong"
+                        )
+                    }
+
+                    is Response.Success<*> -> {
+                        _state.value = GetImagesState(
+                            isLoading = false,
+                            image = it.data ?: emptyList()
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}
