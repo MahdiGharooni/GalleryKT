201e0a1ddf39a8de6b5d7dfd430b5f9dd7746bc7
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-11-20 11:45:55 +0330

    bug fix api

--- app/src/main/java/ir/mahdi/gharooni/gallerykt/data/remote/GalleryAPI.kt
+++ app/src/main/java/ir/mahdi/gharooni/gallerykt/data/remote/GalleryAPI.kt
@@ -4,11 +4,16 @@ import ir.mahdi.gharooni.gallerykt.data.remote.dto.ImageDto
 import ir.mahdi.gharooni.gallerykt.utils.TOKEN
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GalleryAPI {
 
     @Headers("Authorization: $TOKEN")
     @GET("photos")
-    suspend fun getAllImages(): List<ImageDto>
+    suspend fun getAllImages(
+        @Query("page") page:Int,
+        @Query("per_page") perPage:Int,
+    ): List<ImageDto>
 
 }
