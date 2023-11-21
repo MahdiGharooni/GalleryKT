864bd00bb18cd356990beb0f12689154a9b59ad1
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-11-21 16:31:16 +0330

    imp getImages state

--- app/src/main/java/ir/mahdi/gharooni/gallerykt/presentation/state/GetImagesState.kt
+++ app/src/main/java/ir/mahdi/gharooni/gallerykt/presentation/state/GetImagesState.kt
@@ -1,11 +1,9 @@
 package ir.mahdi.gharooni.gallerykt.presentation.state
 
 import ir.mahdi.gharooni.gallerykt.domain.model.Image
-import ir.mahdi.gharooni.gallerykt.utils.Response
-import kotlinx.coroutines.flow.Flow
 
 class GetImagesState(
-    private val isLoading: Boolean = false,
-    private val image: List<Image> = emptyList(),
-    private val message: String = "",
+     val isLoading: Boolean = false,
+     val images: List<Image> = emptyList(),
+     val message: String = "",
 )
