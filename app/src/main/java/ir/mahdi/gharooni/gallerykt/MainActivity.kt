a6177577aae864ea0faa0c2cbe4bb32f259c1f6f
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-11-22 09:57:18 +0330

    updated UI of images list

--- app/src/main/java/ir/mahdi/gharooni/gallerykt/MainActivity.kt
+++ app/src/main/java/ir/mahdi/gharooni/gallerykt/MainActivity.kt
@@ -3,44 +3,113 @@ package ir.mahdi.gharooni.gallerykt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ir.mahdi.gharooni.gallerykt.ui.theme.GalleryKTTheme
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.android.AndroidEntryPoint
+import ir.mahdi.gharooni.gallerykt.domain.model.Image
+import ir.mahdi.gharooni.gallerykt.presentation.state.GetImagesState
+import ir.mahdi.gharooni.gallerykt.presentation.ui.home.ImageCard
+import ir.mahdi.gharooni.gallerykt.presentation.view_model.GetImagesViewModel
+import ir.mahdi.gharooni.gallerykt.utils.ui.theme.GalleryKTTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GalleryKTTheme {
-                // A surface container using the 'background' color from the theme
+                val viewModel = viewModel<GetImagesViewModel>()
+                val state: State<GetImagesState> = viewModel.state
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    if (state.value.isLoading) {
+                        LoadingView()
+                    } else if (state.value.images.isEmpty()) {
+                        ErrorText(state.value.message)
+                    } else {
+                        ImagesList(state.value.images)
+                    }
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LoadingView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GalleryKTTheme {
-        Greeting("Android")
+fun ErrorText(message: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(alignment = Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+        )
     }
+}
+
+@Composable
+fun ImagesList(images: List<Image>) {
+    LazyColumn(
+        modifier = Modifier
+            .height(200.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+    ) {
+        items(images.size) {
+            if (it == 0) {
+                Spacer(modifier = Modifier.height(24.dp))
+            }
+            ImageCard(image = images[it])
+            if (it == images.size - 1) {
+                Spacer(modifier = Modifier.height(24.dp))
+            }
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/ir/mahdi/gharooni/gallerykt/presentation/ui/home/ImageCard.kt
@@ -0,0 +1,89 @@
+package ir.mahdi.gharooni.gallerykt.presentation.ui.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import ir.mahdi.gharooni.gallerykt.domain.model.Image as galleryImage
+
+@Composable
+fun ImageCard(image: galleryImage) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        modifier = Modifier
+            .height(200.dp)
+            .padding(bottom = 4.dp, top = 4.dp),
+    ) {
+        Box {
+            AsyncImage(
+                model = image.url.regular,
+                contentDescription = image.description,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth,
+            )
+            Footer(image)
+        }
+    }
+}
+
+@Composable
+fun Footer(image: galleryImage) {
+    Surface(
+        modifier = Modifier
+            .offset(x = 0.dp, y = 160.dp)
+            .height(48.dp)
+            .fillMaxWidth(),
+        color = Color.Transparent.copy(alpha = 0.7f)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+        ) {
+            Row {
+                AsyncImage(
+                    model = image.user.profileImage.small,
+                    contentDescription = image.user.name,
+                    modifier = Modifier
+                        .size(21.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = image.user.name)
+            }
+            Row {
+                Text(text = "${image.likes}")
+                Spacer(modifier = Modifier.width(4.dp))
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "",
+                    tint = Color.Red,
+                )
+
+            }
+        }
+    }
+}
