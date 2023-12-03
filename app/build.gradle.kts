33888a266bc2e1b23e4ea929980c0eed5dbf0a1f
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-12-03 15:06:42 +0330

    added coild and navigator library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,9 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("io.coil-kt:coil:2.5.0")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
 
 
 }
